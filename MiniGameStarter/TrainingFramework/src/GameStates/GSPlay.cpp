71f50361c5e31b064cfa075cdf4d34e749ba35c3
phaminh174@gmail.com <phaminh174@gmail.com>
2022-07-04 17:37:58 +0700

    init project

---
+++ MiniGameStarter/TrainingFramework/src/GameStates/GSPlay.cpp
@@ -0,0 +1,170 @@
+#include "GSPlay.h"
+
+#include "Shader.h"
+#include "Texture.h"
+#include "Model.h"
+#include "Camera.h"
+#include "Font.h"
+#include "Sprite2D.h"
+#include "Sprite3D.h"
+#include "Text.h"
+#include "GameButton.h"
+#include "SpriteAnimation.h"
+
+
+
+GSPlay::GSPlay()
+{
+}
+
+
+GSPlay::~GSPlay()
+{
+}
+
+
+void GSPlay::Init()
+{
+	auto model = ResourceManagers::GetInstance()->GetModel("Sprite2D.nfg");
+	auto texture = ResourceManagers::GetInstance()->GetTexture("bg_play1.tga");
+
+	// background
+	auto shader = ResourceManagers::GetInstance()->GetShader("TextureShader");
+	m_background = std::make_shared<Sprite2D>(model, shader, texture);
+	m_background->Set2DPosition((float)Globals::screenWidth / 2, (float)Globals::screenHeight / 2);
+	m_background->SetSize(Globals::screenWidth, Globals::screenHeight);
+
+	// button close
+	texture = ResourceManagers::GetInstance()->GetTexture("btn_close.tga");
+	std::shared_ptr<GameButton>  button = std::make_shared<GameButton>(model, shader, texture);
+	button->Set2DPosition(Globals::screenWidth - 50, 50);
+	button->SetSize(50, 50);
+	button->SetOnClick([this]() {
+			GameStateMachine::GetInstance()->PopState();
+		});
+	m_listButton.push_back(button);
+
+	// score
+	shader = ResourceManagers::GetInstance()->GetShader("TextShader");
+	std::shared_ptr<Font> font = ResourceManagers::GetInstance()->GetFont("Brightly Crush Shine.otf");
+	m_score = std::make_shared< Text>(shader, font, "score: 10", TextColor::RED, 1.0);
+	m_score->Set2DPosition(Vector2(5, 25));
+
+	shader = ResourceManagers::GetInstance()->GetShader("Animation");
+	texture = ResourceManagers::GetInstance()->GetTexture("Actor1_2.tga");
+	std::shared_ptr<SpriteAnimation> obj = std::make_shared<SpriteAnimation>(model, shader, texture, 9, 6, 3, 0.1f);
+	
+	obj->Set2DPosition(240, 400);
+	obj->SetSize(30, 40);
+	m_listAnimation.push_back(obj);
+	m_KeyPress = 0;
+}
+
+void GSPlay::Exit()
+{
+}
+
+
+void GSPlay::Pause()
+{
+}
+
+void GSPlay::Resume()
+{
+}
+
+
+void GSPlay::HandleEvents()
+{
+}
+
+void GSPlay::HandleKeyEvents(int key, bool bIsPressed)
+{
+	if (bIsPressed)
+	{
+		switch (key)
+		{
+		case KEY_MOVE_LEFT:
+			m_KeyPress |= 1;
+			break;
+		case KEY_MOVE_BACKWORD:
+			m_KeyPress |= 1<<1;
+			break;
+		case KEY_MOVE_RIGHT:
+			m_KeyPress |= 1<<2;
+			break;
+		case KEY_MOVE_FORWORD:
+			m_KeyPress |= 1<<3;
+			break;
+		default:
+			break;
+		}
+	}
+	else
+	{
+		switch (key)
+		{
+		case KEY_MOVE_LEFT:
+			m_KeyPress ^= 1;
+			break;
+		case KEY_MOVE_BACKWORD:
+			m_KeyPress ^= 1 << 1;
+			break;
+		case KEY_MOVE_RIGHT:
+			m_KeyPress ^= 1 << 2;
+			break;
+		case KEY_MOVE_FORWORD:
+			m_KeyPress ^= 1 << 3;
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+void GSPlay::HandleTouchEvents(int x, int y, bool bIsPressed)
+{
+	for (auto button : m_listButton)
+	{
+		if(button->HandleTouchEvents(x, y, bIsPressed))
+		{
+			break;
+		}
+	}
+}
+
+void GSPlay::HandleMouseMoveEvents(int x, int y)
+{
+}
+
+void GSPlay::Update(float deltaTime)
+{
+	switch (m_KeyPress)//Handle Key event
+	{
+	default:
+		break;
+	}
+	for (auto it : m_listButton)
+	{
+		it->Update(deltaTime);
+	}
+	for (auto it : m_listAnimation)
+	{
+		it->Update(deltaTime);
+	}
+}
+
+void GSPlay::Draw()
+{
+	m_background->Draw();
+	m_score->Draw();
+	for (auto it : m_listButton)
+	{
+		it->Draw();
+	}
+
+	for (auto it : m_listAnimation)
+	{
+		it->Draw();
+	}
+}
